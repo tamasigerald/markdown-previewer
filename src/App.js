2bcfd19ea90b71d79ee27b14cbacfeb396641940
Gerald Andrei Tamasi <tamasigerald@gmail.com>
2021-03-26 20:58:00 +0100

    initial commit & base skeleton

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+const { default: Edit } = require("components/Edit");
+const { default: Header } = require("components/Header");
+const { default: Layout } = require("components/Layout");
+const { default: Preview } = require("components/Preview");
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <> 
+    <Header />
+    <Layout>
+      <Edit />
+      <Preview />
+    </Layout>
+    </>
   );
 }
 
